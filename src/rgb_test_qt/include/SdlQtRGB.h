98c0e30da459ef02a6ed7ac14fbf2c5365dd8aca
handabao <18862963156@163.com>
2026-02-02 14:06:41 +0800

    qt && sdl2 test for show picture

---
+++ src/rgb_test_qt/include/SdlQtRGB.h
@@ -0,0 +1,26 @@
+﻿#ifndef SDLQTRGB_H
+#define SDLQTRGB_H
+
+#include <QtWidgets/QWidget>
+
+namespace Ui
+{
+    class SdlQtRGBClass;
+}
+
+class SdlQtRGB : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SdlQtRGB(QWidget *parent = Q_NULLPTR);
+    ~SdlQtRGB() override;
+
+public:
+    void timerEvent(QTimerEvent *event) override;
+
+private:
+    Ui::SdlQtRGBClass *ui_ = nullptr;
+};
+
+
+#endif // SDLQTRGB_H
---
+++ src/rgb_test_qt/src/SdlQtRGB.cpp
@@ -0,0 +1,72 @@
+﻿#include "SdlQtRGB.h"
+
+#include "ui_sdlqtrgb.h"
+
+#include <SDL2/SDL.h>
+
+static int sdl_width  = 0;
+static int sdl_height = 0;
+
+static SDL_Window   *sdl_win     = NULL;
+static SDL_Renderer *sdl_render  = NULL;
+static SDL_Texture  *sdl_texture = NULL;
+
+static unsigned char *rgb      = NULL;
+static int            pix_size = 4;
+
+SdlQtRGB::SdlQtRGB(QWidget *parent) : QWidget(parent)
+{
+    ui_ = new Ui::SdlQtRGBClass();
+    ui_->setupUi(this);
+
+    sdl_width  = ui_->label->width();
+    sdl_height = ui_->label->height();
+
+    /// 初始化SDL
+    SDL_Init(SDL_INIT_VIDEO);
+
+    /// 创建窗口
+    sdl_win = SDL_CreateWindowFrom((void *)ui_->label->winId());
+
+    /// 创建渲染器
+    sdl_render = SDL_CreateRenderer(sdl_win, -1, SDL_RENDERER_ACCELERATED);
+
+    /// 创建材质
+    sdl_texture =
+            SDL_CreateTexture(sdl_render, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, sdl_width, sdl_height);
+
+    rgb = new unsigned char[sdl_width * sdl_height * pix_size];
+
+    startTimer(10);
+}
+
+SdlQtRGB::~SdlQtRGB()
+{
+}
+
+void SdlQtRGB::timerEvent(QTimerEvent *event)
+{
+    for (int j = 0; j < sdl_height; j++)
+    {
+        int b = j * sdl_width * pix_size;
+        for (int i = 0; i < sdl_width * pix_size; i += pix_size)
+        {
+            rgb[b + i]     = 0;   ///< B
+            rgb[b + i + 1] = 255; ///< G
+            rgb[b + i + 2] = 0;   ///< R
+            rgb[b + i + 3] = 0;   ///< A
+        }
+    }
+
+    SDL_UpdateTexture(sdl_texture, NULL, rgb, sdl_width * pix_size);
+    SDL_RenderClear(sdl_render);
+    SDL_Rect rect;
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = sdl_width;
+    rect.h = sdl_height;
+    SDL_RenderCopy(sdl_render, sdl_texture, NULL, &rect);
+    SDL_RenderPresent(sdl_render);
+
+    QWidget::timerEvent(event);
+}
