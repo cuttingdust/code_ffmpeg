c89f9beaeea6de36e1d86588f11016b48cbdb096
handabao <18862963156@163.com>
2026-02-26 17:56:19 +0800

    解码 开始解码

--- src/ffmepg_decode/src/main.cpp
+++ src/ffmepg_decode/src/main.cpp
@@ -34,12 +34,16 @@ int main(int argc, char *argv[])
     /// 分割上下文
     auto parser = av_parser_init(codec_id);
     auto pkt    = av_packet_alloc();
+    auto frame  = av_frame_alloc();
     while (!ifs.eof())
     {
         ifs.read((char *)inbuf, sizeof(inbuf));
         int data_size = ifs.gcount(); /// 读取的字节数
         if (data_size <= 0)
+        {
             break;
+        }
+
         auto data = inbuf;
         while (data_size > 0) /// 一次有多帧数据
         {
@@ -51,10 +55,43 @@ int main(int argc, char *argv[])
             data_size -= ret; /// 已处理
             if (pkt->size)
             {
-                std::cout << pkt->size << " " << std::flush;
+                // std::cout << pkt->size << " " << std::flush;
+                /// 发送packet到解码线程
+                ret = avcodec_send_packet(c, pkt);
+                if (ret < 0)
+                {
+                    break;
+                }
+                /// 获取多帧解码数据
+                while (ret >= 0)
+                {
+                    /// 每次回调用av_frame_unref
+                    ret = avcodec_receive_frame(c, frame);
+                    if (ret < 0)
+                        break;
+
+                    std::cout << frame->format << " " << std::flush;
+                }
             }
         }
     }
+
+    ///取出缓存数据
+    int ret = avcodec_send_packet(c, NULL);
+    while (ret >= 0)
+    {
+        ret = avcodec_receive_frame(c, frame);
+        if (ret < 0)
+        {
+            break;
+        }
+
+        std::cout << frame->format << "-" << std::flush;
+    }
+
+    av_parser_close(parser);
+    avcodec_free_context(&c);
+    av_frame_free(&frame);
     av_packet_free(&pkt);
 
     getchar();
