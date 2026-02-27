5baa56b9c66d8845a481c802ee77c8a4f10fc656
handabao <18862963156@163.com>
2026-02-27 10:17:02 +0800

    60完成解码视频渲染并测试多线解码帧率

---
+++ src/ffmepg_decode_view/src/XVideoView.cpp
@@ -0,0 +1,279 @@
+﻿#include "XVideoView.h"
+
+#include "XSDL.h"
+
+#include <iostream>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
+
+#include <mutex>
+#include <fstream>
+
+void MSleep(unsigned int ms)
+{
+    const auto beg = clock();
+    for (int i = 0; std::cmp_less(i, ms); i++)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        if (std::cmp_greater_equal((clock() - beg) / (CLOCKS_PER_SEC / 1000), ms))
+        {
+            break;
+        }
+    }
+}
+
+long long NowMs()
+{
+    return clock() / (CLOCKS_PER_SEC / 1000);
+}
+
+class XVideoView::PImpl
+{
+public:
+    PImpl(XVideoView *owenr);
+    ~PImpl() = default;
+
+public:
+    XVideoView *owenr_   = nullptr;
+    void       *win_id_  = nullptr; ///< 窗口句柄
+    int         width_   = 0;       ///< 材质宽高
+    int         height_  = 0;
+    int         scale_w_ = 0; ///< 显示大小
+    int         scale_h_ = 0;
+    Format      fmt_     = RGBA; ///< 像素格式
+    std::mutex  mtx_;            ///< 确保线程安全
+
+    int       render_fps_ = 0; ///< 显示帧率
+    long long beg_ms_     = 0; ///< 计时开始时间
+    int       count_      = 0; ///< 统计显示次数
+
+    std::ifstream ifs_;
+    AVFrame      *frame_ = nullptr;
+};
+
+XVideoView::PImpl::PImpl(XVideoView *owenr) : owenr_(owenr)
+{
+}
+
+XVideoView::XVideoView() : impl_(std::make_unique<XVideoView::PImpl>(this))
+{
+}
+
+XVideoView::~XVideoView()
+{
+}
+
+auto XVideoView::create(RenderType type) -> XVideoView *
+{
+    switch (type)
+    {
+        case XVideoView::SDL:
+            return new XSDL;
+            break;
+        default:
+            break;
+    }
+    return nullptr;
+}
+
+auto XVideoView::drawFrame(AVFrame *frame) -> bool
+{
+    if (!frame || !frame->data[0])
+    {
+        return false;
+    }
+    impl_->count_++;
+
+    if (impl_->beg_ms_ <= 0)
+    {
+        impl_->beg_ms_ = clock();
+    }
+    /// 计算显示帧率
+    else if ((clock() - impl_->beg_ms_) / (CLOCKS_PER_SEC / 1000) >= 1000) /// 一秒计算一次fps
+    {
+        impl_->render_fps_ = impl_->count_;
+        impl_->count_      = 0;
+        impl_->beg_ms_     = clock();
+    }
+
+    switch (frame->format)
+    {
+        case AV_PIX_FMT_YUV420P:
+            return draw(frame->data[0], frame->linesize[0], /// Y
+                        frame->data[1], frame->linesize[1], /// U
+                        frame->data[2], frame->linesize[2]  /// V
+            );
+        case AV_PIX_FMT_BGRA:
+        case AV_PIX_FMT_ARGB:
+        case AV_PIX_FMT_RGBA:
+            return draw(frame->data[0], frame->linesize[0]);
+        default:
+            break;
+    }
+
+    return false;
+}
+
+auto XVideoView::scale(int w, int h) -> void
+{
+    impl_->scale_w_ = w;
+    impl_->scale_h_ = h;
+}
+
+auto XVideoView::setScaleWidth(int w) -> void
+{
+    impl_->scale_w_ = w;
+}
+
+auto XVideoView::setScaleHeight(int h) -> void
+{
+    impl_->scale_h_ = h;
+}
+
+auto XVideoView::scaleWidth() -> int
+{
+    return impl_->scale_w_;
+}
+
+auto XVideoView::scaleHeight() -> int
+{
+    return impl_->scale_h_;
+}
+
+auto XVideoView::renderFps() const -> int
+{
+    return impl_->render_fps_;
+}
+
+auto XVideoView::open(const std::string &filepath) -> bool
+{
+    if (impl_->ifs_.is_open())
+    {
+        impl_->ifs_.close();
+    }
+    impl_->ifs_.open(filepath, std::ios::binary);
+    return impl_->ifs_.is_open();
+}
+
+auto XVideoView::read() -> AVFrame *
+{
+    if (impl_->width_ <= 0 || impl_->height_ <= 0 || !impl_->ifs_)
+    {
+        return nullptr;
+    }
+
+    /// AVFrame空间已经申请，如果参数发生变化，需要释放空间
+    if (impl_->frame_)
+    {
+        if (impl_->frame_->width != impl_->width_ || impl_->frame_->height != impl_->height_ ||
+            impl_->frame_->format != impl_->fmt_)
+        {
+            /// 释放AVFrame对象空间，和buf引用计数减一
+            av_frame_free(&impl_->frame_);
+        }
+    }
+
+    if (!impl_->frame_)
+    {
+        /// 分配对象空间和像素空间
+        impl_->frame_         = av_frame_alloc();
+        impl_->frame_->width  = impl_->width_;
+        impl_->frame_->height = impl_->height_;
+        impl_->frame_->format = impl_->fmt_;
+
+        if (impl_->frame_->format == AV_PIX_FMT_YUV420P)
+        {
+            impl_->frame_->linesize[0] = impl_->width_;     /// Y
+            impl_->frame_->linesize[1] = impl_->width_ / 2; /// U
+            impl_->frame_->linesize[2] = impl_->width_ / 2; /// V
+        }
+        else
+        {
+            impl_->frame_->linesize[0] = impl_->width_ * 4;
+        }
+        /// 生成AVFrame空间，使用默认对齐
+        auto re = av_frame_get_buffer(impl_->frame_, 0);
+        if (re != 0)
+        {
+            char buf[1024] = { 0 };
+            av_strerror(re, buf, sizeof(buf) - 1);
+            std::cout << buf << std::endl;
+            av_frame_free(&impl_->frame_);
+            return nullptr;
+        }
+    }
+
+    if (!impl_->frame_)
+    {
+        return nullptr;
+    }
+
+
+    /// 读取一帧数据
+    if (impl_->frame_->format == AV_PIX_FMT_YUV420P)
+    {
+        impl_->ifs_.read((char *)impl_->frame_->data[0],
+                         impl_->frame_->linesize[0] * impl_->height_); /// Y
+        impl_->ifs_.read((char *)impl_->frame_->data[1],
+                         impl_->frame_->linesize[1] * (impl_->height_ / 2)); /// U
+        impl_->ifs_.read((char *)impl_->frame_->data[2],
+                         impl_->frame_->linesize[2] * (impl_->height_ / 2)); /// V
+    }
+    else /// RGBA ARGB BGRA 32
+    {
+        impl_->ifs_.read((char *)impl_->frame_->data[0], impl_->frame_->linesize[0] * impl_->height_);
+    }
+
+
+    if (impl_->ifs_.gcount() == 0)
+        return nullptr;
+
+    return impl_->frame_;
+}
+
+auto XVideoView::setWindow(void *win) -> void
+{
+    impl_->win_id_ = win;
+}
+
+auto XVideoView::window() const -> void *
+{
+    return impl_->win_id_;
+}
+
+auto XVideoView::hasWin() -> bool
+{
+    return impl_->win_id_ != nullptr;
+}
+
+auto XVideoView::setWidth(int width) -> void
+{
+    impl_->width_ = width;
+}
+
+auto XVideoView::width() const -> int
+{
+    return impl_->width_;
+}
+
+auto XVideoView::setHeight(int height) -> void
+{
+    impl_->height_ = height;
+}
+
+auto XVideoView::height() const -> int
+{
+    return impl_->height_;
+}
+
+auto XVideoView::setFormat(const Format &fmt) -> void
+{
+    impl_->fmt_ = fmt;
+}
+
+auto XVideoView::format() const -> Format
+{
+    return impl_->fmt_;
+}
---
+++ src/ffmepg_decode_view/src/main.cpp
@@ -0,0 +1,129 @@
+﻿#include "XVideoView.h"
+
+#include <iostream>
+#include <fstream>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavutil/opt.h>
+}
+
+extern long long NowMs();
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL, "zh_CN.UTF-8");
+
+    auto view = XVideoView::create();
+
+    /// 1 分割h264 存入AVPacket
+    /// ffmpeg -i v1080.mp4 -s 400x300 test.h264
+    std::string   filename = "test.h264";
+    std::ifstream ifs(filename, std::ios::binary);
+    if (!ifs)
+    {
+        return -1;
+    }
+
+    unsigned char inbuf[4096] = { 0 };
+    AVCodecID     codec_id    = AV_CODEC_ID_H264;
+
+    /// 1 找解码器
+    auto codec = avcodec_find_decoder(codec_id);
+
+    /// 2 创建上下文
+    auto c = avcodec_alloc_context3(codec);
+
+    c->thread_count = 16;
+    /// 3 打开上下文
+    avcodec_open2(c, NULL, NULL);
+
+    /// 分割上下文
+    auto parser      = av_parser_init(codec_id);
+    auto pkt         = av_packet_alloc();
+    auto frame       = av_frame_alloc();
+    auto begin       = NowMs();
+    int  count       = 0; /// 解码统计
+    bool is_init_win = false;
+    while (!ifs.eof())
+    {
+        ifs.read((char *)inbuf, sizeof(inbuf));
+        int data_size = ifs.gcount(); /// 读取的字节数
+        if (data_size <= 0)
+        {
+            break;
+        }
+
+        auto data = inbuf;
+        while (data_size > 0) /// 一次有多帧数据
+        {
+            /// 通过0001 截断输出到AVPacket 返回帧大小 /// ，作用是从原始字节流中分割出完整的 NALU（网络抽象层单元）。
+            int ret = av_parser_parse2(parser, c, &pkt->data, &pkt->size, /// 输出
+                                       data, data_size,                   /// 输入
+                                       AV_NOPTS_VALUE, AV_NOPTS_VALUE, 0);
+            data += ret;
+            data_size -= ret; /// 已处理
+            if (pkt->size)
+            {
+                // std::cout << pkt->size << " " << std::flush;
+                /// 发送packet到解码线程
+                ret = avcodec_send_packet(c, pkt);
+                if (ret < 0)
+                {
+                    break;
+                }
+                /// 获取多帧解码数据
+                while (ret >= 0)
+                {
+                    /// 每次回调用av_frame_unref
+                    ret = avcodec_receive_frame(c, frame);
+                    if (ret < 0)
+                        break;
+
+                    std::cout << frame->format << " " << std::flush;
+
+                    //////////////////////////////////////////////////////////////////
+
+
+                    /// 第一帧初始化窗口
+                    if (!is_init_win)
+                    {
+                        is_init_win = true;
+                        view->init(frame->width, frame->height, (XVideoView::Format)frame->format);
+                    }
+                    view->drawFrame(frame);
+
+                    count++;
+                    auto cur = NowMs();
+                    if (cur - begin >= 100) /// 1/10秒钟计算一次
+                    {
+                        std::cout << "\nfps = " << count * 10 << std::endl;
+                        count = 0;
+                        begin = cur;
+                    }
+                }
+            }
+        }
+    }
+
+    ///取出缓存数据
+    int ret = avcodec_send_packet(c, NULL);
+    while (ret >= 0)
+    {
+        ret = avcodec_receive_frame(c, frame);
+        if (ret < 0)
+        {
+            break;
+        }
+
+        std::cout << frame->format << "-" << std::flush;
+    }
+
+    av_parser_close(parser);
+    avcodec_free_context(&c);
+    av_frame_free(&frame);
+    av_packet_free(&pkt);
+
+    getchar();
+    return 0;
+}
