78fb0f10c8f8a59487f90697901885df715737cd
handabao <18862963156@163.com>
2026-04-13 18:03:17 +0800

    提升日历

---
+++ src/XView/XCalendar.cpp
@@ -0,0 +1,31 @@
+﻿
+#include "XCalendar.h"
+#include <QPainter>
+XCalendar::XCalendar(QWidget* p) : QCalendarWidget(p)
+{
+}
+void XCalendar::paintCell(QPainter* painter, const QRect& rec, QDate date) const
+{
+    /// 有视频的日期特殊显示
+    /// 测试日期 4号
+    if (date.day() != 4)
+    {
+        QCalendarWidget::paintCell(painter, rec, date);
+        return;
+    }
+    
+    
+    auto font = painter->font();
+    /// 设置字体
+    font.setPixelSize(40);
+    
+    /// 选中状态刷背景色
+    if (date == selectedDate())
+    {
+        painter->setBrush(QColor(118, 178, 224)); /// 刷子颜色
+        painter->drawRect(rec);                   /// 绘制背景
+    }
+    painter->setFont(font);             /// 设置字体和颜色
+    painter->setPen(QColor(255, 0, 0)); /// 字颜色
+    painter->drawText(rec, Qt::AlignCenter, QString::number(date.day()));
+}
---
+++ src/XView/XCalendar.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <QCalendarWidget>
+class XCalendar : public QCalendarWidget
+{
+public:
+    XCalendar(QWidget* p);
+    void paintCell(QPainter* painter, const QRect& rec, QDate date) const override;
+};
